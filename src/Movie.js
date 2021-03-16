184ea9787d4fa1de1f68b7f07eee58df8c3a4efe
meno1011 <39692905+meno1011@users.noreply.github.com>
2021-03-16 18:52:36 +0900

    #4.2 styling web

--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import Proptypes from 'prop-types';
+import React from "react";
+import Proptypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}){
-    return <h4>{title}</h4>
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div class="movies__movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie__data">
+        <h3 class="movie__title" style={{}}>{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.proTypes = {
-    id : Proptypes.number.isRequired,
-    year : Proptypes.number.isRequired,
-    title : Proptypes.string.isRequired,
-    summary : Proptypes.string.isRequired,
-    poster : Proptypes.string.isRequired,
+  id: Proptypes.number.isRequired,
+  year: Proptypes.number.isRequired,
+  title: Proptypes.string.isRequired,
+  summary: Proptypes.string.isRequired,
+  poster: Proptypes.string.isRequired,
 };
 
 export default Movie;
