5315275f054c83e4c3db201e4be2edb5e737f338
meno1011 <39692905+meno1011@users.noreply.github.com>
2021-03-17 19:11:06 +0900

    modified code a little bit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Navigation.css";
 
 function Navigation(){
-    return (<div>
+    return (<div className="nav">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-    </div>)
-};
+    </div>);
+}
 
 export default Navigation;
